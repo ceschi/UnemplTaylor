3d5a39c7cd5e7341c8a58451233f86b9e6fbfda3
ceschi <emanuele.franceschi@gmail.com>
2017-11-22 17:40:02 +0100

    longer loop

--- inflanalysis.R
+++ inflanalysis.R
@@ -64,6 +64,7 @@ if (flag___optilag==1){
 }
 
 for (i in 1:n){
+  ##### Unit root tests #####
   inflation[['unitroot']][[i]] <- ur.df(na.omit(pi[,i]),
                                          # DF test, max lag to consider
                                         lags=llags,
@@ -91,74 +92,60 @@ for (i in 1:n){
               cat(paste0('For ', names(pi)[i], ' it is possible to reject \nthe null hypothesis of unit root at 99%.\n\n'))
   }
   
-}
-
-##### AR regression with fixed lags k=5 #####
-
-for (i in 1:n){
+  
+  
+  ##### AR regression with fixed lags k=5 #####
   inflation[['ark']][[i]] <- lm(data= (pi[,i] %>% lagger(lag=k)),
                                 formula=formula.maker(df=pi[,i] %>% lagger(lag=k),
                                                       y= pi[,i] %>% lagger(lag=k) %>% 
                                                         names(.) %>% first())
-                                ) %>% summary() %>% coef()
+  ) %>% summary() %>% coef()
   cat('\n\n')
   print(paste0(inflation$names[[i]],',  ', k, ' exogenously defined lags'))
   print(inflation[['ark']][[i]])
-}
-
-
-##### AR regression with optimal lags #####
-if (flag___optilag==1){
-for (i in 1:n){
-  inflation[['aroptilm']][[i]] <- rolloop(df = pi[,i], window = wind, lags = inflation[['aropti']][[i]])
   
-  inflation[['plot_aropti']][[i]] <- ggplot(data=inflation[['aroptilm']][[i]],
-                                            aes(x=index(inflation[['aroptilm']][[i]]),
-                                                y=inflation[['aroptilm']][[i]][,r]))+
-    # plot the above with line geom, in black
-    geom_line(colour='black', size=1)+
-    # adds upper confidence band in red
-    geom_line(aes(y=inflation[['aroptilm']][[i]][,r]+inflation[['aroptilm']][[i]][,inflation[['aropti']][[i]]+r]),
-              colour='red')+
-    # adds lower confidence band in red
-    geom_line(aes(y=inflation[['aroptilm']][[i]][,r]-inflation[['aroptilm']][[i]][,inflation[['aropti']][[i]]+r]),
-              colour='red')+
-    # adds unit root line
-    geom_line(aes(y=1), colour='black', size=.8)+
-    # plot makeup
-    geom_smooth(method='loess', colour='blue')+scale_x_yearqtr(format='%Y Q%q', n=20)+theme_bw()+
-    scale_y_continuous()+xlab(' ') + ylab(paste0('AR(',r,') coeff. estimates')) + 
-    ggtitle(paste0(inflation$names[[i]],' - ', inflation[['aropti']][[i]], ' optimal lags'))
-  
-  if  (flag___plot==0) plot(inflation[['plot_aropti']][[i]])
-  
-  # saves graphs in proper directory with names
-  ggsave(paste0('AR(',r,') coeff. estimates ', inflation[['names']][[i]], 
-                ' - ', inflation[['aropti']][[i]], ' optimal lags.pdf'),
-         inflation[['plot_aropti']][[i]],
-         device='pdf',
-         graphs_dir,
-         height=8, width=14.16, units='in')
-}
-}
-
-##### AR(k) rolling window regressions #####
-## consider using map() or apply() function families
-## to vectorise code and make it faster. This requires
-## wrapping loop's actions in an ad-hoc function
-
-for (i in 1:n){
-
+  ##### AR regression with optimal lags #####
+  if (flag___optilag==1){
+    inflation[['aroptilm']][[i]] <- rolloop(df = pi[,i], window = wind, lags = inflation[['aropti']][[i]])
+    
+    inflation[['plot_aropti']][[i]] <- ggplot(data=inflation[['aroptilm']][[i]],
+                                              aes(x=index(inflation[['aroptilm']][[i]]),
+                                                  y=inflation[['aroptilm']][[i]][,r]))+
+      # plot the above with line geom, in black
+      geom_line(colour='black', size=1)+
+      # adds upper confidence band in red
+      geom_line(aes(y=inflation[['aroptilm']][[i]][,r]+inflation[['aroptilm']][[i]][,inflation[['aropti']][[i]]+r]),
+                colour='red')+
+      # adds lower confidence band in red
+      geom_line(aes(y=inflation[['aroptilm']][[i]][,r]-inflation[['aroptilm']][[i]][,inflation[['aropti']][[i]]+r]),
+                colour='red')+
+      # adds unit root line
+      geom_line(aes(y=1), colour='black', size=.8)+
+      # plot makeup
+      geom_smooth(method='loess', colour='blue')+scale_x_yearqtr(format='%Y Q%q', n=20)+theme_bw()+
+      scale_y_continuous()+xlab(' ') + ylab(paste0('AR(',r,') coeff. estimates')) + 
+      ggtitle(paste0(inflation$names[[i]],' - ', inflation[['aropti']][[i]], ' optimal lags'))
+    
+    if  (flag___plot==0) plot(inflation[['plot_aropti']][[i]])
+    
+    # saves graphs in proper directory with names
+    ggsave(paste0('AR(',r,') coeff. estimates ', inflation[['names']][[i]], 
+                  ' - ', inflation[['aropti']][[i]], ' optimal lags.pdf'),
+           inflation[['plot_aropti']][[i]],
+           device='pdf',
+           graphs_dir,
+           height=8, width=14.16, units='in')
+  }
+  ##### AR(k) rolling window regressions #####
+  ## consider using map() or apply() function families
+  ## to vectorise code and make it faster. This requires
+  ## wrapping loop's actions in an ad-hoc function
+  ## about window length: 58 is used by Pivetta&Reis as well as Fuhrer, trade off
+  ## is about precision and extension of the resulting estimates
   inflation[['rollm']][[i]] <- rolloop(df = pi[,i], window = wind, lags = k)
-
-}
-## about window length: 58 is used by Pivetta&Reis as well as Fuhrer, trade off
-## is about precision and extension of the resulting estimates
-
-
-##### Plots registration #####
-
-for (i in 1:n){
+  
+  
+  ##### Plots registration #####
   # attaches data, select first column of estimates and time
   inflation[['plot_rollm']][[i]] <- ggplot(data=inflation[['rollm']][[i]],
                                            aes(x=index(inflation[['rollm']][[i]]),
@@ -170,7 +157,7 @@ for (i in 1:n){
               colour='red')+
     # adds lower confidence band in red
     geom_line(aes(y=inflation[['rollm']][[i]][,r]-inflation[['rollm']][[i]][,k+r]),
-                  colour='red')+
+              colour='red')+
     # adds unit root line
     geom_line(aes(y=1), colour='black', size=.8)+
     # plot makeup
@@ -186,16 +173,6 @@ for (i in 1:n){
          graphs_dir,
          height=8, width=14.16, units='in')
 }
-
-
-
-
-
-
-
-
-
-
 
 
 ##### Housekeeping ####
