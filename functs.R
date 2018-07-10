2591e4f4ff26a6845e8452919013252fb4f2cc0e
ceschi <emanuele.franceschi@gmail.com>
2018-07-10 18:53:02 +0200

    fixed tbls in fredr

--- functs.R
+++ functs.R
@@ -548,7 +548,7 @@ pkgs <- c('vars', 'glue', 'MSwM', 'lazyeval',
           'mFilter', 'fredr',
           'readr', 'quantmod',
           'devtools', 'lubridate',
-          'readxl', 'VARsignR')
+          'readxl', 'VARsignR', 'tbl2xts')
 # fill pkgs with names of the packages to install
 
 devtools::install_github('sboysel/fredr')
